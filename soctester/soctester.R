2a270171d679a4c691ff31e3a24fa38bd05f2616
tomthe <tom-github@theilemail.de>
2021-06-07 16:14:59 +0200

    tester first commit

---
+++ soctester/soctester.R
@@ -0,0 +1,20 @@
+test_all <- function() {
+  
+  rootfolder <-"D:\\dev\\r\\socsimprojects";
+  folder <- c("CousinDiversity", "MarriageSqueeze", "sweden_small", "sweden_small")
+  supfile <- c("CousinDiversity.sup", "squeeze.sup", "simul_test.sup", "simul_Sweden.sup")
+  seed <- c(12345,1234,1234,1234)
+  df <- data.frame(folder,supfile,seed)
+  
+  run1simulation(rootfolder,folder[1],supfile[1],seed[1]);
+  
+}
+
+run1simulation <- function(rootfolder,folder, supfile,seed) {
+  library(rsoc)
+  
+  setwd(paste0(rootfolder,"\\",folder[1]))
+  startSocsimWithFile(supfile,seed)
+  
+}
+test_all()
