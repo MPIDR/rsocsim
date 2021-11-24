fbe9bebed0ba1446b5ef743445cde3df34a96819
tomthe <tom-github@theilemail.de>
2021-11-25 00:47:15 +0100

    apply instead of futures...

--- R/startsocsim.R
+++ R/startsocsim.R
@@ -25,3 +25,35 @@ run1simulationwithfile <- function(rootfolder,folder, supfile,seed) {
     v1 <- future::value(f1)
   })
 }
+
+#' Run a socsim-simulation in the r-process
+#'
+#' @param rootfolder rootfolder  name of the simulation
+#' @param supfile the .sup file to start the simulation
+#' @param seed RNG seed
+#' @return The results will be written into the specified folder
+#' @export
+run1simulationwithfile_inprocess <- function(rootfolder, supfile,seed) {
+  # use the "future" library to run a rcpp-socsim simulation
+  # in a seperate process
+  print("Start run1simulationwithfile. in process.")
+  print(rootfolder)
+  setwd(paste0(rootfolder))
+  print(seed)
+  startSocsimWithFile(supfile,seed)
+}
+
+
+#library(parallel)
+
+#' @export
+run1simulationwithfile_apply <- function(folder, supfile,seed="23") {
+  # use the "future" library to run a rcpp-socsim simulation
+  # in a seperate process
+  #folder = "D:\\dev\\r\\socsimprojects\\CousinDiversity"
+  #supfile = "CousinDiversity.sup"
+  #seed = "23"
+  numCores=1
+  parallel::mclapply(c(1),run1simulationwithfile_inprocess(folder, supfile,seed), mc.cores = numCores)
+  
+}
