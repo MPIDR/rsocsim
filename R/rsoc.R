2c676796621523d1552160130968bd9cc2a6870a
tomthe <tom-github@theilemail.de>
2023-01-10 15:49:30 +0100

    rename file rsoc.R to rsocsim.R

--- R/rsoc.R
+++
@@ -1,7 +0,0 @@
-#' @useDynLib rsocsim
-#' @importFrom Rcpp evalCpp
-#' @exportPattern "^[[:alpha:]]+"
-NULL
-
-# This file is necessary so that Roxigen2 and devtools::document() generate the correct NAMESPACE file.
-# See https://stackoverflow.com/questions/33395264/trouble-building-r-package-wtih-devtools-when-it-uses-rcpparmadillo
