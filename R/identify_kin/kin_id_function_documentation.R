67242b9e76fee8184654ae456cd8bd48f8c17a06
Tom Theile <tom-github@theilemail.de>
2023-03-03 14:53:56 +0100

    refactor provided function. not working yet

--- R/identify_kin/kin_id_function_documentation.R
+++ R/identify_kin/kin_id_function_documentation.R
@@ -1,3 +1,17 @@
+res$ggparents <- lapply(pid,
+  function(pid){
+      as.vector(unlist(opop[opop$pid == pid, 
+        c("MMM","MMF","MFM","MFF",
+        "FMM","FMF","FFM","FFF")]))})
+
+if(kin_by_sex){
+  res$ggmothers <- lapply(pid, function(pid){
+     as.vector(unlist(opop[opop$pid == pid, c("MMM","FMM", "FFM", "MFM")]))})
+  
+  res$ggfathers <- lapply(pid, function(pid){
+    as.vector(unlist(opop[opop$pid == pid, c("FFF", "MMF", "MFF", "FMF")]))})
+}
+
 #' Identify members of a kin network for an individual or individuals of 
 #' interest. 
 #' @param opop An R object from SOCSIM microsimulation output (population file). 
