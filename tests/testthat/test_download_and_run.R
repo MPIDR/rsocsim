366eecec4f795eda9eaf099976f9081251f9a044
Tom Theile <tom-github@theilemail.de>
2025-03-14 16:15:40 +0100

    Add tests for reading simulation outputs and estimating fertility rates

--- tests/testthat/test_download_and_run.R
+++ tests/testthat/test_download_and_run.R
@@ -18,4 +18,36 @@ test_that("Download rates and run simulation", {
   print(paste("Result:", result))
   # Add assertions based on expected results from the simulation
   expect_is(result, 1)
+
+  
+  # read simulation outputs
+  omar = rsocsim::read_omar(folder,supfile,seed,suffix=suffix)
+  omar
+
+  opop = rsocsim::read_opop(folder,supfile,seed,suffix)
+  opop
+
+  # Obtain partial kinship network, with omar and opop already in R environment
+  pid <- c("111", "10211", "10311")
+  kin_network <- getKin(opop = opop, omar = omar, pid = pid, 
+                        extra_kintypes = c("unclesaunts", "niblings"), kin_by_sex = TRUE)
+
+  kin_network$nieces[[3]][1]
+
+  opop$pid[kin_network$nieces[[3]]]
+
+
+  ####
+
+  fert_rates <- estimate_fertility_rates(
+    opop=opop,
+    final_sim_year=1966,
+    year_min=1963,
+    year_max=1965,
+    year_group = 5,
+    age_min_fert = 15,
+    age_max_fert = 50,
+    age_group = 5
+  )
+  fert_rates
 })
