bd912964862aefe61710955ba781457133faf4cd
Tom Theile <tomthe@users.noreply.github.com>
2025-07-02 12:35:27 +0200

    Bump version to 1.7.3 and update RoxygenNote to 7.3.2 in DESCRIPTION file; refactor test for download rates to improve clarity and consistency

--- tests/testthat/test_download_and_run.R
+++ tests/testthat/test_download_and_run.R
@@ -6,15 +6,17 @@ test_that("Download rates and run simulation", {
   tmpdir <- file.path(getwd(), "tmp/tmp")
   
   expect_true(dir.exists(tmpdir))
-  
-  rsocsim::download_rates(tmpdir, "MM", yearStart = 1997, yearEnd = 2010, source = "UN")
+
+  countrycode <- "MM"
+  final_sim_year <- 2010
+  rsocsim::download_rates(tmpdir, countrycode, yearStart = 2003, yearEnd = final_sim_year, source = "UN")
   
   # Assuming the download_rates function creates a specific file, check if it exists
-  expect_true(file.exists(file.path(tmpdir, "rates", "MM_rates_info.sup")))
+  expect_true(file.exists(file.path(tmpdir, "rates", paste0(countrycode, "_rates_info.sup"))))
   
   rsocsim::create_initial_population(tmpdir, size_opop = 1000, output_base = "presim")
 
-  result <- rsocsim::socsim(tmpdir, "MM_rates_info.sup")
+  result <- rsocsim::socsim(tmpdir, paste0(countrycode, "_rates_info.sup"))
   print(paste("Result:", result))
   # Add assertions based on expected results from the simulation
   expect_is(result, 1)
@@ -22,10 +24,10 @@ test_that("Download rates and run simulation", {
   
   # read simulation outputs
   omar = rsocsim::read_omar(folder,supfile,seed,suffix=suffix)
-  omar
+  omar.head()
 
   opop = rsocsim::read_opop(folder,supfile,seed,suffix)
-  opop
+  opop.head()
 
   # Obtain partial kinship network, with omar and opop already in R environment
   pid <- c("111", "10211", "10311")
@@ -41,13 +43,13 @@ test_that("Download rates and run simulation", {
 
   fert_rates <- estimate_fertility_rates(
     opop=opop,
-    final_sim_year=1966,
-    year_min=1963,
-    year_max=1965,
+    final_sim_year=final_sim_year,
+    year_min=2004,
+    year_max=2009,
     year_group = 5,
     age_min_fert = 15,
     age_max_fert = 50,
     age_group = 5
   )
-  fert_rates
+  print(fert_rates.head())
 })
